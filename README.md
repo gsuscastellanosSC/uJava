328d94be3dddddfdde64c768406484924c4eeb45
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-11-15 21:59:19 -0500

    Classs#82
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

--- README.md
+++ README.md
@@ -215,6 +215,8 @@ https://www.udemy.com/course/universidad-java-especialista-en-java-desde-cero-a-
     ***80. Arreglos en Java***
 ***Class#81**
     ***81. Arreglos en Java - parte 2***
+***Class#82***
+    ***82. Arreglos en Java - parte 3***
 ***Links***
     Arrays:
         https://github.com/gsuscastellanosSC/uJava/blob/main/src/fundamentosJava/Arrays.java
--- src/fundamentosJava/Arrays.java
+++ src/fundamentosJava/Arrays.java
@@ -15,10 +15,16 @@ public class Arrays {
 
     public static void main(String[] args) {
         int ages[] = new int[3];
+        System.out.print("[");
         for (int i = 0; i < ages.length; i++) {
-            ages[i] = (1+i)+10 * i;
-            System.out.println(ages[i]);
+            ages[i] = (1 + i) + 10 * i;
+            if (i < ages.length - 1) {
+                System.out.print(ages[i] + ", ");
+            } else {
+                System.out.print(ages[i]);
+            }
+
         }
-        
+        System.out.println("]");
     }
 }
