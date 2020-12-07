89aae605522a17ac32fbb04e26e39b5a46b6f8f6
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-12-06 21:27:23 -0500

    Class#175
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

---
+++ JDBC/ManagementJDBC/src/main/java/data/ConnectionClass.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sc
+ */
+public class ConnectionClass {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "GsusC292020*";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+    }
+
+    public static void close(ResultSet rs) throws SQLException {
+        rs.close();
+    }
+
+    public static void close(Statement statement) throws SQLException {
+        statement.close();
+    }
+
+    public static void close(PreparedStatement preparedStatement) throws SQLException {
+        preparedStatement.close();
+    }
+
+    public static void clonse(Connection connection) throws SQLException {
+        connection.close();
+    }
+}
---
+++ JDBC/ManagementJDBC/src/main/java/test/TestMySqlJDBC.java
@@ -0,0 +1,39 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author sc
+ */
+public class TestMySqlJDBC {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/test?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url, "sc", "GsusC292020*");
+            Statement instruction = connection.createStatement();
+            String sql = "select id_person, name, lastName, mail, phone from person";
+            ResultSet result = instruction.executeQuery(sql);
+            while (result.next()) {
+                System.out.println("\nId Person: " + result.getInt("id_person")
+                        + ", Nombre: " + result.getString("name")
+                        + ", Apellido: " + result.getString("lastName")
+                        + ", Correo:" + result.getString("mail")
+                        + " Telefono:" + result.getString("phone"));
+            }
+            result.close();
+            instruction.close();
+            connection.close();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
